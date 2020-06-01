12a79735d511bc6eec8e10989b033b9183779c04
Nicholas Labarre <bitbucket@ndlr.xyz>
2020-06-01 13:35:54 -0400

    start breach unit tests

--- test/hibp_client/breach_test.clj
+++ test/hibp_client/breach_test.clj
@@ -1,3 +1,34 @@
 (ns hibp-client.breach-test
   (:require [clojure.test :refer :all]
-            [hibp-client.breach :refer :all]))
+            [hibp-client.breach :refer :all]
+            [hibp-client.api :as api]))
+
+(deftest get-for-account-test
+  (testing "uses renamed query params"
+    (with-redefs [api/get-json-body
+                  (fn [_ {:keys [query-params]}]
+                    (is (contains? query-params :truncateResponse)))]
+      (get-for-account "api-key" "test@example.com" {:truncate-response true})))
+
+  (testing "uses api key"
+    (with-redefs [api/get-json-body
+                  (fn [_ {:keys [api-key]}]
+                    (is (= api-key "api-key")))]
+      (get-for-account "api-key" "test@example.com")))
+
+  (testing "builds account path"
+    (with-redefs [api/get-json-body
+                  (fn [path _]
+                    (is path "/breachedaccount/test@example.com"))]
+      (get-for-account "api-key" "test@example.com"))))
+
+(deftest get-all-test
+  (testing "gets breaches path")
+  (testing "specifies domain in query params if present"))
+
+(deftest get-for-name-test
+  (testing "checks that name is present")
+  (testing "builds breach path"))
+
+(deftest get-data-classes-test
+  (testing "gets dataclasses path"))
