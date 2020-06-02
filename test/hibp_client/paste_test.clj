534c5a0f75a690e63aade25a06c5c7437f21a3c5
Nicholas Labarre <bitbucket@ndlr.xyz>
2020-06-01 21:54:00 -0400

    complete paste tests

--- test/hibp_client/paste_test.clj
+++ test/hibp_client/paste_test.clj
@@ -1,3 +1,20 @@
 (ns hibp-client.paste-test
   (:require [clojure.test :refer :all]
-            [hibp-client.paste :refer :all]))
+            [hibp-client.paste :refer :all]
+            [hibp-client.test-fixtures :as test-fixtures]))
+
+(use-fixtures :once test-fixtures/redefine-get-json-body)
+
+(deftest get-for-account-test
+  (testing "checks that account is specified"
+    (is (thrown? AssertionError (get-for-account "api-key" nil))))
+
+  (testing "builds path from account"
+    (is (=
+          (:path (get-for-account "api-key" "test@example.org"))
+          "/pasteaccount/test@example.org")))
+
+  (testing "uses api key"
+    (is (=
+          (:configuration (get-for-account "api-key" "test@example.org"))
+          {:api-key "api-key"}))))
